10ce28c7c4964e99c99679effd701da1b06b40c7
unknown <shoudou@outlook.com>
2019-11-02 18:39:00 -0500

    added google maps

--- app/src/main/java/com/se340/smartshoveler/RegisterActivity.java
+++ app/src/main/java/com/se340/smartshoveler/RegisterActivity.java
@@ -2,10 +2,12 @@ package com.se340.smartshoveler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -13,17 +15,27 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        final EditText etFullname = (EditText) findViewById(R.id.etName);
-        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
-        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
-        final Button btnRegister = (Button) findViewById(R.id.btnRegister);
+        final EditText etFullname = findViewById(R.id.etName);
+        final EditText etEmail = findViewById(R.id.etEmail);
+        final EditText etPassword = findViewById(R.id.etPassword);
+        final Button btnRegister = findViewById(R.id.btnRegister);
 
-        btnRegister.setOnClickListener(new View.OnClickListener(){
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 final String name = etFullname.getText().toString();
                 final String email = etEmail.getText().toString();
                 final String password = etPassword.getText().toString();
+            }
+        });
+
+        final TextView loginLink = findViewById(R.id.tvRegisterToLogin);
+
+        loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                RegisterActivity.this.startActivity(registerIntent);
             }
         });
     }
