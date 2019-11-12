ca1b094adbdb8dc2fb3fe11964a9404ceb7fb0dd
msandwidi <shoudou@outlook.com>
2019-11-12 15:30:46 -0600

    fixed home activity backbutton

--- app/src/main/java/com/se340/smartshoveler/MainActivity.java
+++ app/src/main/java/com/se340/smartshoveler/MainActivity.java
@@ -12,6 +12,14 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
