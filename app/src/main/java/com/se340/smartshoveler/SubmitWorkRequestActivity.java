9fad218c5cb6e105845af498bde4bb1fd461afad
msandwidi <shoudou@outlook.com>
2019-11-12 17:25:47 -0600

    added field for work request

--- app/src/main/java/com/se340/smartshoveler/SubmitWorkRequestActivity.java
+++ app/src/main/java/com/se340/smartshoveler/SubmitWorkRequestActivity.java
@@ -3,6 +3,7 @@ package com.se340.smartshoveler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SubmitWorkRequestActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class SubmitWorkRequestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_work_request);
+
+        final Button btnSearch = findViewById(R.id.btnSubmitWork);
+
+
+
     }
 }
